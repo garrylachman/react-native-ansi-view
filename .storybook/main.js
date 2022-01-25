083774fce1f0b40e2f089d4129f16c655446b9cb
Garry Lachman <garry@lachman.co>
2022-01-25 20:22:35 +0200

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-controls',
+    '@storybook/addon-knobs',
+  ],
+  stories: ['../src/**/*.stories.@(tsx|jsx)'],
+};
